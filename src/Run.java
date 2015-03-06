1f391e6f458531facea672228cd57b5c6bf01762
Magdalen Berns <m.berns@thismagpie.com>
2015-03-06 16:47:20 +0000

    Add run

---
+++ src/Run.java
@@ -0,0 +1,10 @@
+
+public class Run {
+
+  public static void main(String[] args) {
+    int size = Integer.parseInt(args[0]);
+    double p1 = Double.parseDouble(args[1]);
+    double p2 = Double.parseDouble(args[2]);
+    double p3 = Double.parseDouble(args[3]);
+  }
+}
