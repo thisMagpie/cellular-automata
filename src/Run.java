eb1e56262a21f5d346116e2f8ebf44350712f337
Magdalen Berns <m.berns@thismagpie.com>
2015-03-13 11:53:59 +0000

    add plotting cycles

--- src/Run.java
+++ src/Run.java
@@ -16,23 +16,68 @@ public class Run {
       for (int j = 0; j < size; j++) {
           draw.paintPixels(box[i][j], i,j);
           draw.repaint();
-
       }
     }
 
     boolean run = true;
-    while (run) {
-      int[] spins = SIR.picks(2, size);
-      box[spins[0]][spins[1]] = SIR.update(box,
-                                           p1,
-                                           p2,
-                                           p3,
-                                           spins[0],
-                                           spins[1]);
-      draw.paintPixels(box[spins[0]][spins[1]],
-                          spins[0],
-                          spins[1]);
-      draw.repaint();
+    run(draw, run, size, box, p1, p2,p3);
+
+    int noSweeps = 10; // number of times to sweep through
+    int eq = 100; // number of equilibration cycles
+    int n = 100; // number of data points to measure
+
+    for (int i = 0; i < noSweeps; i++) {
+      box = sweep(box, p1, p2, p3);
+      for (int j = 0; j < eq; j ++)
+        box = sweep(box, p1, p2, p3);
+      // Start taking measurements
+      for (int j = 0; j <= n; j++) {
+        sweep(box, p1, p2, p3);
+        if(j%10==0)
+        n++;
+        // calculate mean
+      }
     }
+  }
+
+  public static void run(DrawSIRS draw,
+                         boolean run,
+                         int size,
+                         int[][] box,
+                         double p1,
+                         double p2,
+                         double p3){
+    while (run) {
+        int[] spins = SIR.picks(2, size);
+        box[spins[0]][spins[1]] = SIR.update(box,
+                                             p1,
+                                             p2,
+                                             p3,
+                                             spins[0],
+                                             spins[1]);
+        draw.paintPixels(box[spins[0]][spins[1]],
+                            spins[0],
+                            spins[1]);
+        draw.repaint();
+      }
+  }
+
+  public static int[][] sweep(int[][] box,
+                           double p1,
+                           double p2,
+                           double p3) {
+
+  for (int i = 0; i < box.length; i++) {
+    for (int j = 0; j < box.length; j++) {
+       int[] spins = SIR.picks(2, box.length);
+       box[spins[0]][spins[1]] = SIR.update(box,
+                                            p1,
+                                            p2,
+                                            p3,
+                                            spins[0],
+                                            spins[1]);
+      }
+    }
+    return box;
   }
 }
